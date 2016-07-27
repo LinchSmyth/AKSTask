2eaa6802822329a34a268a57a50c39e35a8b22fc
LinchSmyth <andrei95-1995@mail.ru>
2016-07-27 19:16:38 +0300

    Generate room_channel, add it to routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
 
   root 'home#show'
 
+  # mount ActionCable for our rooms
+  mount ActionCable.server => '/cable'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
