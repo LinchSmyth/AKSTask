7bcd985837e4a4cca74dca8669d3e07a771e10e0
LinchSmyth <andrei95-1995@mail.ru>
2016-07-27 22:59:53 +0300

    Add unfinished new version of channels

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'rooms/show'
+
   get 'about', to: 'about#show'
 
   root 'home#show'
+
+  mount ActionCable.server => '/cable'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
