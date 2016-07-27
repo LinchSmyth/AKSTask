d3b25ac158a02ed4c7636a8ec92e72ffdc393489
LinchSmyth <andrei95-1995@mail.ru>
2016-07-27 21:56:27 +0300

    Finish ActionCable example (not working)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+
+  Rails.application.config.action_cable.allowed_request_origins = ['http://localhost:3002']
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
