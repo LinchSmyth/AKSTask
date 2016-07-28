ad94641367063e7eedf5df0a3eeed2e226bc566f
LinchSmyth <andrei95-1995@mail.ru>
2016-07-28 21:01:34 +0300

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :password, presence: true, length: { minimum: 6, maximum: 20 }
+
+  has_secure_password
+  validates :password, length: { minimum: 6 }
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160728132423) do
+ActiveRecord::Schema.define(version: 20160728172458) do
 
   create_table "messages", force: :cascade do |t|
     t.text     "content"
@@ -20,9 +20,9 @@ ActiveRecord::Schema.define(version: 20160728132423) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "password"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
