61c67de61832bff90b15370827c2352e9b4aa1d6
LinchSmyth <andrei95-1995@mail.ru>
2016-07-30 18:56:32 +0300

    Add Rooms model

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,5 +2,29 @@ class RoomsController < ApplicationController
   def show
     @messages = Message.all
   end
+  #TODO: remove functional from above (don't forget about view silly!)
+  def index
+    @chat_rooms = Room.all
+  end
+
+  def new
+    @chat_room = Room.new
+  end
+
+  def create
+    @chat_room = current_user.chat_rooms.build(chat_room_params)
+    if @chat_room.save
+      flash[:success] = 'Chat room added!'
+      redirect_to rooms_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def chat_room_params
+    params.require(:chat_room).permit(:title)
+  end
 end
 
