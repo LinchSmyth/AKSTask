cab7fe60cf883ad161ef13c48f6d325ddfa52c78
LinchSmyth <andrei95-1995@mail.ru>
2016-08-05 19:15:48 +0300

    Remove useless routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
-  match '/signup',  to: 'users#new',            via: 'get'
+
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
 
-  # get 'rooms/show'
   resources :rooms, only: [:new, :create, :show, :index]
-  root 'rooms#index'
-  # TODO: rewrite this line and root
 
   get 'about', to: 'about#show'
 
